801cf5feeac35df4a709d0591bd1b9a4050f8bcb
aneeshkumaryadav <192607573+aneeshkumaryadav@users.noreply.github.com>
2026-08-02 00:14:00 +0530

    Add the autoInput Function which automatically invoked when it detect user doesn't pass any password

--- Projects/PasswordValidator.py
+++ Projects/PasswordValidator.py
@@ -1,20 +1,32 @@
 import string, random
-def passwordValidator(password : str):
+def passwordValidator(password : str) -> str:
     upperCase = lowerCase = digit = special = 0
     for char in password:
         if(char.isupper()): upperCase+=1
         elif(char.islower()): lowerCase+=1
         elif(char.isdecimal()): digit+=1
         else: special+=1
-    if(((upperCase>=1 and lowerCase>=1) and (digit>=1 and special>=1)) and len(password) >= 8):
+
+    if(((upperCase<=2 or lowerCase<=2) and (digit>=2 or special>=2)) and (len(password) < 12 and len(password) >= 8)):
+        return f"Medium"
+
+    if(((upperCase>3 or lowerCase>3) or (digit>2 and special>3)) and len(password) >= 12):
         return f"Strong"
+    
     else:
         return f"Weak"
-# print(passwordValidator("eLm@18&Bs"))
-chars = string.ascii_letters + string.digits + string.punctuation
-password = ""
-size = int(input("Enter The Size of Password : "))
-while(len(password) < size):
-    password += random.choice(chars)
-print(password)
-print(passwordValidator(password))
+
+def autoInput():
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+    size = int(input("Enter The Size of Password : "))
+    while(len(password) < size):
+        password += random.choice(chars)
+    print(password)
+    print(passwordValidator(password))
+
+userpass = input("Enter The Password : ")
+if userpass == "":
+    autoInput()
+else:
+    print(passwordValidator(userpass))
