8ecbafc76c019631ae6f28da95453efe66ee807e
aneeshkumaryadav <192607573+aneeshkumaryadav@users.noreply.github.com>
2026-08-02 13:44:31 +0530

    Update the encode with random module and Add the decoder for decode the encoded message

--- Projects/EncodeMessage.py
+++ Projects/EncodeMessage.py
@@ -1,11 +1,32 @@
 import string
+import random
+
+def encoder(message: str, number: int) -> str:
+    result = ""
+    for char in message:
+        if(char == " "):
+            continue
+        indexing = string.ascii_lowercase.index(char)
+        indexing = (indexing+number)%26
+        result += string.ascii_lowercase[indexing]
+
+    return result
+
+def decoder(message: str, number: int) -> str:
+    result = ""
+    for char in message:
+        if(char == " "):
+            continue
+        indexing = string.ascii_lowercase.index(char)
+        indexing = (indexing-number)%26
+        result += string.ascii_lowercase[indexing]
+
+    return result
+
 
 message = input("Enter The Message : ").lower()
-result = ""
-for char in message:
-    if(char == " "):
-        continue
-    indexing = string.ascii_lowercase.index(char)
-    indexing = (indexing+3)%26
-    result += string.ascii_lowercase[indexing]
-print(result)
+number = random.randint(1,26)
+
+encodedMessage = encoder(message, number)
+print(f"{encodedMessage}")
+print(f"{decoder(encodedMessage, number)}")
