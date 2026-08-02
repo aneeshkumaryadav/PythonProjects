9cbcffa5afa50d88a112b314ab937f5e19ef90a6
aneeshkumaryadav <192607573+aneeshkumaryadav@users.noreply.github.com>
2026-08-02 20:44:55 +0530

    Add the Result Table which showing points of each player

--- Projects/RockPaperScissor.py
+++ Projects/RockPaperScissor.py
@@ -3,6 +3,7 @@ import random
 print("R for Rock")
 print("P for Paper")
 print("S for Scissor")
+print()
 
 compWinPoint = 0
 userWinPoint = 0
@@ -17,21 +18,34 @@ while(numbersofGame > 0):
         if(computerChoice==userInput):
             compWinPoint += 1
             userWinPoint += 1
+            print(f"Match Draw 😎 +1 Points To Each")
+            print()
 
         elif((computerChoice=="r" and userInput=="p") or (computerChoice=="p" and userInput=="s") or computerChoice=="s" and userInput=="r"):
             userWinPoint += 1
+            print(f"User Wins 😁 +1 Point To User")
+            print()
 
         else:
             compWinPoint += 1
+            print(f"Computer Wins 😒 +1 Point To Computer")
+            print()
+
         numbersofGame -= 1
     else:
         print("Enter Valid Character")
-    
+
+print("*"*5, "RESULT TABLE", "*"*5)
+print(f"User : {userWinPoint}")
+print(f"Computer : {compWinPoint}")
+print("*"*24)
+print()
+
 if(compWinPoint > userWinPoint):
-    print(f"Computer Wins 😒 with {compWinPoint} Points")
+    print(f"Computer Wins 😒 with {abs(compWinPoint - userWinPoint)} Points")
 
 elif(compWinPoint == userWinPoint):
-    print(f"Match Draw 😎 with {compWinPoint} Points")
+    print(f"Match Draw 😎 with 0 Points")
 
 else:
-    print(f"User Wins 😁 with {userWinPoint} Points")
+    print(f"User Wins 😁 with {abs(userWinPoint - compWinPoint)} Points")
